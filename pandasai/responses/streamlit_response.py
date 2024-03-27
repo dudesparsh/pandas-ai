c0974aa8a700f69a1e84b08c0ece78f68437e9d6
dudesparsh <qwope@rediffmail.com>
2024-03-27 11:38:58 +0530

    updating google gemini and the related unit tests

--- pandasai/responses/streamlit_response.py
+++ pandasai/responses/streamlit_response.py
@@ -5,17 +5,29 @@ import pandas as pd
 from pandasai.responses.response_parser import ResponseParser
 
 
+from PIL import Image
+
 class StreamlitResponse(ResponseParser):
     def __init__(self, context):
         super().__init__(context)
 
+    # def format_plot(self, result) -> None:
+    #     """
+    #     Display plot against a user query in Streamlit
+    #     Args:
+    #         result (dict): result contains type and value
+    #     """
+    #     return result["value"]
+        
+        # Take 1
     def format_plot(self, result) -> None:
-        """
-        Display plot against a user query in Streamlit
-        Args:
-            result (dict): result contains type and value
-        """
+    
+        # if self._context._config.open_charts:
+        #     with Image.open(result["value"]) as img:
+        #         img.show()
+
         return result["value"]
+
 
     def format_dataframe(self, result: dict) -> pd.DataFrame:
         """
---
+++ tests/unit_tests/llms/test_google_gemini.py
@@ -0,0 +1,107 @@
+"""Unit tests for the openai LLM class"""
+import re
+
+import pytest
+from google import generativeai
+
+from pandasai.exceptions import APIKeyNotFoundError
+from pandasai.llm import GoogleGemini
+from pandasai.prompts import AbstractPrompt
+
+class MockedCompletion:
+    def __init__(self, result: str):
+        self.result = result
+
+class TestGoogleGemini:
+    """Unit tests for the Google Gemini LLM class"""
+
+    @pytest.fixture
+    def prompt(self):
+        class MockAbstractPrompt(AbstractPrompt):
+            template: str = "Hello"
+
+        return MockAbstractPrompt()
+
+    def test_type_without_token(self):
+        with pytest.raises(APIKeyNotFoundError):
+            GoogleGemini(api_key="")
+
+    def test_type_with_token(self):
+        assert GoogleGemini(api_key="test").type == "google-gemini"
+
+    def test_params_setting(self):
+        llm = GoogleGemini(
+            api_key="test",
+            model="models/gemini-pro",
+            temperature=0.5,
+            top_p=1.0,
+            top_k=50,
+            max_output_tokens=64,
+        )
+
+        assert llm.model == "models/gemini-pro"
+        assert llm.temperature == 0.5
+        assert llm.top_p == 1.0
+        assert llm.top_k == 50
+        assert llm.max_output_tokens == 64
+
+    def test_validations(self, prompt):
+        with pytest.raises(
+            ValueError, match=re.escape("temperature must be in the range [0.0, 1.0]")
+        ):
+            GoogleGemini(api_key="test", temperature=-1).call(prompt, "World")
+
+        with pytest.raises(
+            ValueError, match=re.escape("temperature must be in the range [0.0, 1.0]")
+        ):
+            GoogleGemini(api_key="test", temperature=1.1).call(prompt, "World")
+
+        with pytest.raises(
+            ValueError, match=re.escape("top_p must be in the range [0.0, 1.0]")
+        ):
+            GoogleGemini(api_key="test", top_p=-1).call(prompt, "World")
+
+        with pytest.raises(
+            ValueError, match=re.escape("top_p must be in the range [0.0, 1.0]")
+        ):
+            GoogleGemini(api_key="test", top_p=1.1).call(prompt, "World")
+
+        with pytest.raises(
+            ValueError, match=re.escape("top_k must be in the range [0.0, 100.0]")
+        ):
+            GoogleGemini(api_key="test", top_k=-100).call(prompt, "World")
+
+        with pytest.raises(
+            ValueError, match=re.escape("top_k must be in the range [0.0, 100.0]")
+        ):
+            GoogleGemini(api_key="test", top_k=110).call(prompt, "World")
+
+        with pytest.raises(
+            ValueError, match=re.escape("max_output_tokens must be greater than zero")
+        ):
+            GoogleGemini(api_key="test", max_output_tokens=0).call(prompt, "World")
+
+        with pytest.raises(ValueError, match=re.escape("model is required.")):
+            GoogleGemini(api_key="test", model="").call(prompt, "World")
+
+    def test_text_generation(self, mocker):
+        llm = GoogleGemini(api_key="test")
+        expected_text = "This is the expected text."
+        expected_response = MockedCompletion(expected_text)
+        mocker.patch.object(
+            generativeai, "generate_text", return_value=expected_response
+        )
+
+        result = llm._generate_text("Hi")
+        assert result == expected_text
+
+    def test_call(self, mocker, prompt):
+        llm = GoogleGemini(api_key="test")
+        expected_text = "This is the expected text."
+        expected_response = MockedCompletion(expected_text)
+        mocker.patch.object(
+            generativeai, "generate_text", return_value=expected_response
+        )
+
+        result = llm.call(instruction=prompt, suffix="!")
+        assert result == expected_text
